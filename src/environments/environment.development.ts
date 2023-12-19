424054538c9c712be3642aa603382bb40e0afc4f
ghost9742 <milosavljevic.nemanja87@gmail.com>
2023-12-19 03:17:31 +0100

    firebase implementation

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyCLOtEn_sH1gKvxz7KizQWEO1ojs9lKQcY',
+    authDomain: 'clips-a7528.firebaseapp.com',
+    projectId: 'clips-a7528',
+    storageBucket: 'clips-a7528.appspot.com',
+    appId: '1:349686185022:web:4d7bfafd4d3151bd14a56f',
+  },
+};
